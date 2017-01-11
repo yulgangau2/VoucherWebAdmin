aa050495bcf73587e91b49d75ff770e74d5ae461
yulgangau2 <alsomsau@gmail.com>
2017-01-11 13:19:24 +0700

    add heads

--- Voucher/Details.php
+++ Voucher/Details.php
@@ -7,7 +7,7 @@ include ("connectdb.php");
 <!DOCTYPE html>
 <htmls>
 
-<head>
+<heads>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EnglishVoucher_31AUG</title>
